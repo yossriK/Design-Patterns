d45e2531d1ae06d23225d9677da29c4a36669d26
yossriK <yossri.khalil@gmail.com>
2021-06-20 14:55:16 -0600

    add singleton pattern

---
+++ SingletonPattern/SingletonPattern/SingletonImplementationExamples/StaticSingleton.cs
@@ -0,0 +1,11 @@
+﻿namespace SingletonPattern
+{
+    // this is hte another example of creating a thread safe implementation of singleton class
+    public class StaticSingleton
+    {
+        public static StaticSingleton Instance { get; } = new StaticSingleton();
+
+        private StaticSingleton() { }
+    }
+  
+}
